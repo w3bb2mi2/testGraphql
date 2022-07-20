9e67845013e6b5cbee1c1e3df1085c71b1825ab5
w3bb2mi2 <w3bb2mi2@gmail.com>
2022-07-20 07:57:32 +0300

    first commit

--- server/server/app.js
+++ server/server/app.js
@@ -2,16 +2,19 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('../schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 const app = express();
 const PORT = 3005;
 
-mongoose.connect('mongodb://Yauhen:Pass123@ds163835.mlab.com:63835/graphql-tutorial', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/graphql', { useNewUrlParser: true });
 
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true,
 }));
+
+app.use(cors())
 
 const dbConnection = mongoose.connection;
 dbConnection.on('error', err => console.log(`Connection error: ${err}`));
